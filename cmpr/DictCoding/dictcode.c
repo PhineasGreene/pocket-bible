5841ee778639cc54d49f5df5182323be5e6aa11c
PhineasGreene <phineasgreene@outlook.com>
2021-12-14 18:36:26 -0600

    dict enc done

--- cmpr/DictCoding/dictcode.c
+++ cmpr/DictCoding/dictcode.c
@@ -17,25 +17,26 @@ struct dict {
 
 // encode a file from a word list
 void encFile(char** wl, int wllen, FILE* input, FILE* output);
-int encWord(char** wl, int wllen, char* word);
+// write a file heading including the word list and encoded word size
+void writeHeader(char** wl, int wllen, FILE* output);
+// return the index of a word in a word list
+unsigned int encWord(char** wl, int wllen, char* word);
 // write a word [length] bits to [file], containing the value [value],
 // using [byte] as a buffer, with [pos] being the current position in [byte]
-void writeWord(int value, int length, char* byte, int* pos, FILE* file);
-
+void writeWord(unsigned int value, int length, char* byte, int* pos, FILE* file);
 // calculate the minimum number of bits needed to encode n words
 int minBits(int words); 
-
 // create an array from a dictionary
 void buildWordList(char** wl, struct dict d); 
+// free dictionary
 void destroyDict(struct dict* d);
 // fill a dictionary with the unique words in file
 void buildDict(struct dict* d, FILE* file); 
-
+// is [word] in dictionary
 bool inDict(struct dict d, char* word);
 // append a word to a dictionary
 void appendWord(struct dict* d, char* word); 
 void printDict(struct dict d);
-
 // letters and digits
 bool isWordChar(char c); 
 // strictly word terminators (newline and space)
@@ -76,6 +77,9 @@ int main(int argc, char** argv){
 	// free the memory we allocated for dictionary
 	destroyDict(&dictionary);
 
+	rewind(input);
+	encFile(wordList, length, input, output);
+
 	fclose(input);
 	fclose(output);
 
@@ -83,11 +87,18 @@ int main(int argc, char** argv){
 }
 
 void encFile(char** wl, int wllen, FILE* input, FILE* output){
+	writeHeader(wl, wllen, output);
+
 	char c;
 	char wordBuff[MAX_WORD];
 	int wordIndex = 0;
 
-	while((c = getc(file)) != EOF){
+	int size = minBits(wllen);
+
+	char byte;
+	int pos = 0;
+
+	while((c = getc(input)) != EOF){
 		if(isWordChar(c)){
 			// add char to word buffer
 			wordBuff[wordIndex] = c;
@@ -96,19 +107,23 @@ void encFile(char** wl, int wllen, FILE* input, FILE* output){
 		}else if(isWordTerminator(c)){
 			// add word to dictionary
 			if(wordIndex > 0){
-				//!!!
+				unsigned int code = encWord(wl, wllen, wordBuff);
+				writeWord(code, size, &byte, &pos, output);
 				wordIndex = 0;
 				wordBuff[0] = '\0';
 			}
 		}else if(isSingleCharWord(c)){
 			// add word buffer word and single char word
 			if(wordIndex > 0){
-				//!!!
+				unsigned int code = encWord(wl, wllen, wordBuff);
+				writeWord(code, size, &byte, &pos, output);
 			}
 
 			wordBuff[0] = c;
 			wordBuff[1] = '\0';
-			//!!!
+
+			unsigned int code = encWord(wl, wllen, wordBuff);
+			writeWord(code, size, &byte, &pos, output);
 
 			wordIndex = 0;
 			wordBuff[0] = '\0';
@@ -116,11 +131,37 @@ void encFile(char** wl, int wllen, FILE* input, FILE* output){
 	}
 }
 
-void writeWord(int value, int length, char* byte, int* pos, FILE* file){
+void writeHeader(char** wl, int wllen, FILE* output){
+	fprintf(output, "%d\n", wllen);
 
+	for(int i = 0; i < wllen; i++){
+		fprintf(output, "%s ", wl[i]);
+	}
+
+	putc('\n', output);
 }
 
-int encWord(char** wl, int wllen, char* word){
+void writeWord(unsigned int value, int length, char* byte, int* pos, FILE* file){
+	int intBits = sizeof(int) * 8;
+	unsigned int mask = 1 << (intBits - 1);
+
+	value <<= intBits - length;
+
+	for(int i = 0; i < length; i++){
+		*byte <<= 1;
+		*pos += 1;
+		*byte += (value & mask) >> (intBits - 1);
+		value <<= 1;
+
+		if(*pos == 8){
+			putc(*byte, file);
+			*pos = 0;
+			*byte = 0;
+		}
+	}
+}
+
+unsigned int encWord(char** wl, int wllen, char* word){
 	for(int i = 0; i < wllen; i++){
 		if(strcmp(wl[i], word) == 0) return i;
 	}
